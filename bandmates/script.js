2e4aaa6dc38470164649bacc6be98f4b8c46f85b
AudreyGold04 <audreygoldberg11@gmail.com>
2024-12-16 16:15:48 +0100

    ajout lien vers formulaire de contact

--- bandmates/script.js
+++ bandmates/script.js
@@ -1,22 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('section'); 
   
-    
     const changeBackgroundOnScroll = () => {
-      const scrollPosition = window.scrollY + window.innerHeight / 2; 
-      sections.forEach(section => {
-        const rect = section.getBoundingClientRect();
-        const sectionTop = rect.top + window.scrollY;
-        const sectionBottom = sectionTop + section.offsetHeight;
-  
-       
-        if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
-          section.style.backgroundColor = section.dataset.bgColor; 
-        }
-      });
+        const scrollPosition = window.scrollY + window.innerHeight / 2; 
+        sections.forEach(section => {
+            const rect = section.getBoundingClientRect();
+            const sectionTop = rect.top + window.scrollY;
+            const sectionBottom = sectionTop + section.offsetHeight;
+
+            if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
+                section.style.backgroundColor = section.dataset.bgColor; 
+            }
+        });
     };
-  
-    
+
     document.addEventListener('scroll', changeBackgroundOnScroll);
     changeBackgroundOnScroll(); 
-  });
+});
