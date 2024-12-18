8dc2e3c225c521b54f42e7e777201002e12ce514
AudreyGold04 <audreygoldberg11@gmail.com>
2024-12-18 22:44:46 +0100

    CSS VRAIMENT fixé

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php get_header(); ?>
 
-c<!DOCTYPE html>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>BandMates</title>
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="/wordpress/wp-content/themes/BANDMATES./style.css">
 </head>
 <body>
     <header class="header">
         <div>
-          <img class="logo" src="images/black white Thunder logo.svg">
+          <img class="logo" src="/wordpress/wp-content/themes/BANDMATES./images/black white Thunder logo.svg">
         </div>
         <div class="nav">
           <button class="btn faq">FAQ</button>
@@ -30,14 +29,13 @@ c<!DOCTYPE html>
           </div>
         </div>
         <div class="image-container">
-        <img src="<?php echo wp_get_attachment_image_url(7) ?>" alt="Description image 1" class="img-fluid rounded me-2" style="width: 1366px">
    
         </div>
       </section>
     
       <section class="white-section" data-bg-color="#E5E5E5">
         <h2>Trouvez des musiciens compatibles avec vous.</h2>
-        <img class="musiciens-compatibles"  src="<?php echo get_template_directory_uri (); ?> images/musiciens.png" alt="Groupe de musiciens">
+        <img class="musiciens-compatibles"  src="/wordpress/wp-content/themes/BANDMATES./images/musiciens.png" alt="Groupe de musiciens">
       </section>
     
       <section class="black-section" data-bg-color="black">
@@ -46,7 +44,7 @@ c<!DOCTYPE html>
           Trouvez des musiciens qui vibrent comme vous. Formez votre groupe, partagez votre passion et jouez ensemble.
         </p>
         
-        <img class="chanteuse" src="images/chanteuse.png" alt="Chanteuse">
+        <img class="chanteuse" src="/wordpress/wp-content/themes/BANDMATES./images/chanteuse.png" alt="Chanteuse">
       </section>
     
       <section class="features" data-bg-color="#E5E5E5">
@@ -71,10 +69,9 @@ c<!DOCTYPE html>
         <h2>C'est le moment de passer à l'action</h2>
         <p>Faites le premier pas aujourd’hui et transformez votre rêve en réalité.</p>
         <button class="btn join">Rejoignez gratuitement</button>
-        <img class="bouche" src="images/bouche.png"
-        <img class="bouche" src="images/bouche.png">
+        <img class="bouche" src="/wordpress/wp-content/themes/BANDMATES./images/bouche.png">
       </section>
-      <img src="<?php echo site_url(); ?>/wp-content/themes/votre-theme/images/logo.png" alt="Logo">
+      <img src="/wordpress/wp-content/themes/BANDMATES./images/black white Thunder logo.svg" alt="Logo">
   <script src="script.js"></script>
 </body>
 </html>
