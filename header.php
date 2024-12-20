ed526dc3eb0da20329fc2ad6d89d0d6e3bcd0b99
AudreyGold04 <audreygoldberg11@gmail.com>
2024-12-20 23:35:32 +0100

    mise en page + typo fixé

--- header.php
+++ header.php
@@ -1,13 +1,20 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-  <?php wp_body_open(); ?>
-
- 
+<!-- <!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>BandMates</title>
+        <link rel="stylesheet" href="/wordpress/wp-content/themes/BANDMATES./style.css">
+    </head>
+    <body>
+        <header class="header">
+            <div>
+                <img class="logo" src="/wordpress/wp-content/themes/BANDMATES./images/black white Thunder logo.svg">
+            </div>
+            <div class="nav">
+                <a class="btn">FAQ</a>
+                <a class="btn">Support</a>
+            </div>
+        </header>
+    </body>
+</html> -->
--- index.php
+++ index.php
@@ -8,29 +8,19 @@
         <title>BandMates</title>
         <link rel="stylesheet" href="/wordpress/wp-content/themes/BANDMATES./style.css">
     </head>
-    <body>
-        <header class="header">
-            <div>
-                <img class="logo" src="/wordpress/wp-content/themes/BANDMATES./images/black white Thunder logo.svg">
-            </div>
-            <div class="nav">
-                <button class="btn">FAQ</button>
-                <button class="btn">Support</button>
-            </div>
-        </header>
-        
+    <body>        
         <section class="black-section first">
             
             <div class="left-content">
-                <h1>La musique vous réunit, BandMates vous connecte</h1>
+                <h1 class="txt">La musique vous réunit,<br>BandMates vous connecte</h1>
                 <div class="">
                     <a href="/wordpress/wp-content/themes/BANDMATES./bandmates/formulaire-inscription.html" class="btn">Rejoindre</a>
-                    <button class="btn">Se connecter</button>
+                    <a class="btn">Se connecter</a>
                 </div>
             </div>
             
             <div class="right-content">
-                <img class="band" src="/wordpress/wp-content/themes/BANDMATES./images/home.png">
+                <img class="band" src="/wordpress/wp-content/themes/BANDMATES./images/home.jpg">
                 <img class="star star-1" src="/wordpress/wp-content/themes/BANDMATES./images/etoiles.png">
                 <img class="star star-2" src="/wordpress/wp-content/themes/BANDMATES./images/etoiles.png">
             </div>
@@ -44,9 +34,8 @@
         <section class="black-section">
             <h2>Formez votre groupe parfait</h2>
             <p>
-            Trouvez des musiciens qui vibrent comme vous. Formez votre groupe, partagez votre passion et jouez ensemble.
+                Trouvez des musiciens qui vibrent comme vous. Formez votre groupe, partagez votre passion et jouez ensemble.
             </p>
-        
             <img class="chanteuse" src="/wordpress/wp-content/themes/BANDMATES./images/chanteuse.png" alt="Chanteuse">
         </section>
     
@@ -71,7 +60,7 @@
         <section class="black-section" data-bg-color="black">
             <h2>C'est le moment de passer à l'action</h2>
             <p>Faites le premier pas aujourd’hui et transformez votre rêve en réalité.</p>
-            <button class="btn">Rejoignez gratuitement</button>
+            <a class="btn">Rejoignez gratuitement</a>
             <img class="bouche" src="/wordpress/wp-content/themes/BANDMATES./images/bouche.png">
         </section>
         <img src="/wordpress/wp-content/themes/BANDMATES./images/black white Thunder logo.svg" alt="Logo">
